45f5dc3cbcc5696beb1bbae5acb36cb34aca7e2a
Ben <ben@scruffy.(none)>
2012-09-30 13:36:25 -0400

    Removed some old code that was breaking cmd.js if executed during irc.connect()

--- plugins/cmd.js
+++ plugins/cmd.js
@@ -4,24 +4,6 @@ var stream = myirc2.getStream();
 var readline = require("readline");
 var LOG_PREFIX = "plugins/cmd.js:\t\t";
 stream.emit("log",LOG_PREFIX + "Starting");
-stream.on("irc.registered",function(m) {
-	if(!authed) {
-		stream.emit("log",LOG_PREFIX+"Sending identify tokens for user: " + config.irc.identUser);
-		stream.emit("client.say","nickserv","identify " + config.irc.identUser + " " + config.irc.identPass);
-	}
-});
-stream.on("irc.notice",function (nick, to, text, message) {
-	nick = unescape(nick);
-	text = unescape(text);
-	if(nick == "NickServ" && text.match(/^You are now identified for/) != null && !authed) {
-		authed = true;
-		stream.emit("log",LOG_PREFIX + "Successfully identified as: " + config.irc.identUser);
-		for(var c in config.irc.channels) {
-			stream.emit("log",LOG_PREFIX + "Joining " + config.irc.channels[c]);
-			stream.emit("client.join",config.irc.channels[c]);
-		}
-	}
-});
 var rl = readline.createInterface(process.stdin,process.stdout,null,true);
 rl.setPrompt("# ", 2);
 rl.prompt();
