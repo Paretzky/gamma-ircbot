160c50b6be90d70f295ae6e0e151b60640533083
Ben <ben@scruffy.(none)>
2012-09-30 18:34:09 -0400

    Added vimeo plugin

---
+++ plugins/vimeo.js
@@ -0,0 +1,24 @@
+var myirc2 = require("../includes/myirc2-helper.js");
+var vimeo = require("vimeo")();
+var url = require("url");
+var config = myirc2.config;
+var stream = myirc2.getStream();
+var LOG_PREFIX = "plugins/vimeo.js:\t";
+stream.emit("log",LOG_PREFIX + "Starting");
+stream.on("irc.message",function(nick,to,text,message) {
+	nick = unescape(nick);
+	to = unescape(to);
+	text = unescape(text);
+	var m = text.indexOf("vimeo.com\/");
+	if(m != -1) {
+		text = text.substring(m).split(/\s+/)[0];
+		text = text.split(/\//).splice(-1);
+		vimeo.video(text,function(err,res) {
+			if(res != null) {
+				res = res[0];
+				stream.emit("log", LOG_PREFIX+"Previewed link " + text + " in " + to);
+				stream.emit("client.say",to,"\"" + res.title + "\" uploaded by " + res.user_name);
+			}
+		});
+	}
+});
