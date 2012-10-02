c9b0ee16c84f1326398fc472cb00a7320ad5626d
Ben <ben@scruffy.(none)>
2012-10-02 07:40:32 -0400

    Added a default timeout of 2.5s to qalc calculations.  Tunable in plugins/qalc.js, for now

--- plugins/qalc.js
+++ plugins/qalc.js
@@ -3,6 +3,7 @@ var config = myirc2.config;
 var stream = myirc2.getStream();
 var LOG_PREFIX = "plugins/qalc.js:\t";
 var spawn = require('child_process').spawn;
+var timeoutInS = 2.5;
 stream.emit("log",LOG_PREFIX + "Starting");
 stream.on("irc.message", function(nick,to,text,message) {
 	nick = unescape(nick);
@@ -13,12 +14,15 @@ stream.on("irc.message", function(nick,to,text,message) {
 			var maths = text.split(/\s+/).splice(1).join(" ").trim();
 			stream.emit("log",LOG_PREFIX + nick + " triggered in " + to + " with expression: " + maths);
 			var p = spawn("qalc",["-t", maths],{ stdio: "pipe" });
+			setTimeout(function(){
+				p.kill('SIGKILL');
+				stream.emit("log", LOG_PREFIX+"Query timed out: " + maths);
+				stream.emit("client.say",to,"Query timed out: " + maths);
+			},timeoutInS*1000);
+	
 			p.stdout.on("data",function(d) {
 				stream.emit("log",LOG_PREFIX + "Finished with expression: " + maths);
 				stream.emit("client.say",to,d.toString().trim());
-			});
-			p.stderr.on("data",function(d) {
-				//stream.emit("log",LOG_PREFIX + "Error: " + d.toString());
 			});
 		}
 	}
