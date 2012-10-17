05e17d5f8a98eb141a601443ecec1a12b8041c44
Ben <ben@scruffy.(none)>
2012-10-17 07:46:39 -0400

    Added user definable precision to @calc

--- plugins/qalc.js
+++ plugins/qalc.js
@@ -11,9 +11,15 @@ stream.on("irc.message", function(nick,to,text,message) {
 	text = unescape(text);
 	if(to.substring(0,1) == "#") {
 		if(text.match(/^\@calc\s/) != null) {
-			var maths = text.split(/\s+/).splice(1).join(" ").trim();
+			var maths = text.split(/\s+/).splice(1);
+			var prec = 5;
+			if(maths[0].match(/^\d+(p|P)$/)  != null) {
+				prec = maths[0].substring(0,maths[0].length - 1);
+				maths = maths.splice(1);
+			}
+			maths = maths.join(" ");
 			stream.emit("log",LOG_PREFIX + nick + " triggered in " + to + " with expression: " + maths);
-			var p = spawn("qalc",["-t", maths],{ stdio: "pipe" });
+			var p = spawn("qalc",["-t", "-set", "PRECISION\ " + prec, maths],{ stdio: "pipe" });
 			var toID = setTimeout(function(){
 				p.kill('SIGKILL');
 				stream.emit("log", LOG_PREFIX+"Query timed out: " + maths);
